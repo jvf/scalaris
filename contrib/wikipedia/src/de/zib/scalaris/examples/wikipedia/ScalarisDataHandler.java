jvf/scalaris

commit 5d30a851ae74845675efa8dc8d227ffc3f003e08

- Wiki on Scalaris:  * implemented page list partitioning with hashes (only the direct import XML->Scalaris from the Wiki servlet creates such partitioned page lists at the moment)  * replaced WIKI_USE_NEW_SCALARIS_OPS with more fine-grained selections of optimisations
git-svn-id: https://scalaris.googlecode.com/svn/trunk@3003 71766da9-9752-0410-ab17-bfafa751c56a

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/ScalarisDataHandler.java
@@ -51,54 +51,6 @@ import de.zib.scalaris.examples.wikipedia.data.SiteInfo;
 public class ScalarisDataHandler {
     
     /**
-     * Different types of DB operations.
-     * 
-     * @author Nico Kruber, kruber@zib.de
-     */
-    public enum ScalarisOpType {
-        /**
-         * Operation involving a (central) page list.
-         */
-        PAGE_LIST,
-        /**
-         * Operation involving a category page list.
-         */
-        CATEGORY_PAGE_LIST,
-        /**
-         * Operation involving a template page list.
-         */
-        TEMPLATE_PAGE_LIST,
-        /**
-         * Operation involving a backlink page list.
-         */
-        BACKLINK_PAGE_LIST,
-        /**
-         * Operation involving a list of (short) revisions.
-         */
-        SHORTREV_LIST,
-        /**
-         * Operation involving the article counter.
-         */
-        ARTICLE_COUNT,
-        /**
-         * Operation involving a wiki page.
-         */
-        PAGE,
-        /**
-         * Operation involving a wiki page revision.
-         */
-        REVISION,
-        /**
-         * Operation involving a contribution.
-         */
-        CONTRIBUTION,
-        /**
-         * Operation involving the edit stats.
-         */
-        EDIT_STAT;
-    }
-    
-    /**
      * Gets the key to store {@link SiteInfo} objects at.
      * 
      * @return Scalaris key
@@ -470,7 +422,8 @@ public class ScalarisDataHandler {
         for (int i = MyNamespace.MIN_NAMESPACE_ID; i < MyNamespace.MAX_NAMESPACE_ID; ++i) {
             scalaris_keys.add(getPageListKey(i));
         }
-        return getPageList2(connection, scalaris_keys, false, "page list");
+        return getPageList2(connection, ScalarisOpType.PAGE_LIST,
+                scalaris_keys, false, "page list");
     }
 
     /**
@@ -484,7 +437,8 @@ public class ScalarisDataHandler {
      * @return a result object with the page list on success
      */
     public static ValueResult<List<String>> getPageList(int namespace, Connection connection) {
-        return getPageList2(connection, getPageListKey(namespace), false, "page list:" + namespace);
+        return getPageList2(connection, ScalarisOpType.PAGE_LIST,
+                Arrays.asList(getPageListKey(namespace)), false, "page list:" + namespace);
     }
 
     /**
@@ -501,8 +455,9 @@ public class ScalarisDataHandler {
      */
     public static ValueResult<List<String>> getPagesInCategory(Connection connection,
             String title, final MyNamespace nsObject) {
-        return getPageList2(connection, getCatPageListKey(title, nsObject),
-                false, "pages in " + title);
+        return getPageList2(connection, ScalarisOpType.CATEGORY_PAGE_LIST,
+                Arrays.asList(getCatPageListKey(title, nsObject)), false,
+                "pages in " + title);
     }
 
     /**
@@ -519,8 +474,9 @@ public class ScalarisDataHandler {
      */
     public static ValueResult<List<String>> getPagesInTemplate(Connection connection,
             String title, final MyNamespace nsObject) {
-        return getPageList2(connection, getTplPageListKey(title, nsObject),
-                true, "pages in " + title);
+        return getPageList2(connection, ScalarisOpType.TEMPLATE_PAGE_LIST,
+                Arrays.asList(getTplPageListKey(title, nsObject)), true,
+                "pages in " + title);
     }
 
     /**
@@ -539,8 +495,9 @@ public class ScalarisDataHandler {
             String title, final MyNamespace nsObject) {
         final String statName = "links to " + title;
         if (Options.WIKI_USE_BACKLINKS) {
-            return getPageList2(connection,
-                    getBackLinksPageListKey(title, nsObject), false, statName);
+            return getPageList2(connection, ScalarisOpType.BACKLINK_PAGE_LIST,
+                    Arrays.asList(getBackLinksPageListKey(title, nsObject)),
+                    false, statName);
         } else {
             return new ValueResult<List<String>>(new ArrayList<String>(0),
                     new ArrayList<String>(0));
@@ -563,8 +520,9 @@ public class ScalarisDataHandler {
         final String statName = "contributions of " + contributor;
         if (Options.WIKI_STORE_CONTRIBUTIONS != STORE_CONTRIB_TYPE.NONE) {
             ValueResult<List<Contribution>> result = getPageList3(connection,
-                    getContributionListKey(contributor), false, statName,
-                    new ErlangConverter<List<Contribution>>() {
+                    ScalarisOpType.CONTRIBUTION,
+                    Arrays.asList(getContributionListKey(contributor)), false,
+                    statName, new ErlangConverter<List<Contribution>>() {
                         @Override
                         public List<Contribution> convert(ErlangValue v)
                                 throws ClassCastException {
@@ -580,32 +538,14 @@ public class ScalarisDataHandler {
                     new ArrayList<String>(0), new ArrayList<Contribution>(0));
         }
     }
-    
-    /**
-     * Retrieves a list of pages from Scalaris.
-     * 
-     * @param connection
-     *            the connection to Scalaris
-     * @param scalaris_key
-     *            the key under which the page list is stored in Scalaris
-     * @param failNotFound
-     *            whether the operation should fail if the key is not found or
-     *            not
-     * @param statName
-     *            name for the time measurement statistics
-     * 
-     * @return a result object with the page list on success
-     */
-    private static ValueResult<List<String>> getPageList2(Connection connection,
-            String scalaris_key, boolean failNotFound, String statName) {
-        return getPageList2(connection, Arrays.asList(scalaris_key), failNotFound, statName);
-    }
 
     /**
      * Retrieves a list of pages from Scalaris.
      * 
      * @param connection
      *            the connection to Scalaris
+     * @param opType
+     *            operation type indicating what is being read
      * @param scalaris_keys
      *            the keys under which the page list is stored in Scalaris
      * @param failNotFound
@@ -616,9 +556,11 @@ public class ScalarisDataHandler {
      * 
      * @return a result object with the page list on success
      */
-    private static ValueResult<List<String>> getPageList2(Connection connection,
-            Collection<String> scalaris_keys, boolean failNotFound, String statName) {
-        ValueResult<List<String>> result = getPageList3(connection,
+    private static ValueResult<List<String>> getPageList2(
+            Connection connection, ScalarisOpType opType,
+            Collection<String> scalaris_keys, boolean failNotFound,
+            String statName) {
+        ValueResult<List<String>> result = getPageList3(connection, opType,
                 scalaris_keys, failNotFound, statName,
                 new ErlangConverter<List<String>>() {
                     @Override
@@ -639,28 +581,8 @@ public class ScalarisDataHandler {
      * 
      * @param connection
      *            the connection to Scalaris
-     * @param scalaris_key
-     *            the key under which the page list is stored in Scalaris
-     * @param failNotFound
-     *            whether the operation should fail if the key is not found or
-     *            not (the value contains null if not failed!)
-     * @param statName
-     *            name for the time measurement statistics
-     * 
-     * @return a result object with the page list on success
-     */
-    private static <T> ValueResult<List<T>> getPageList3(Connection connection,
-            String scalaris_key, boolean failNotFound, String statName,
-            ErlangConverter<List<T>> conv) {
-        return getPageList3(connection, Arrays.asList(scalaris_key), failNotFound, statName, conv);
-    }
-
-    /**
-     * Retrieves a list of pages from Scalaris.
-     * @param <T>
-     * 
-     * @param connection
-     *            the connection to Scalaris
+     * @param opType
+     *            operation type indicating what is being read
      * @param scalaris_keys
      *            the keys under which the page list is stored in Scalaris
      * @param failNotFound
@@ -672,60 +594,34 @@ public class ScalarisDataHandler {
      * @return a result object with the page list on success
      */
     private static <T> ValueResult<List<T>> getPageList3(Connection connection,
-            Collection<String> scalaris_keys, boolean failNotFound,
-            String statName, ErlangConverter<List<T>> conv) {
+            ScalarisOpType opType, Collection<String> scalaris_keys,
+            boolean failNotFound, String statName, ErlangConverter<List<T>> conv) {
         final long timeAtStart = System.currentTimeMillis();
         List<String> involvedKeys = new ArrayList<String>();
+        
         if (connection == null) {
             return new ValueResult<List<T>>(false, involvedKeys,
                     "no connection to Scalaris", true, statName,
                     System.currentTimeMillis() - timeAtStart);
         }
+        
+        final MyScalarisSingleOpExecutor executor = new MyScalarisSingleOpExecutor(
+                new TransactionSingleOp(connection), involvedKeys);
 
-        TransactionSingleOp scalaris_single = new TransactionSingleOp(connection);
-        TransactionSingleOp.ResultList results;
+        final ScalarisReadListOp1<T> readOp = new ScalarisReadListOp1<T>(scalaris_keys,
+                Options.OPTIMISATIONS.get(opType), conv, failNotFound);
+        executor.addOp(readOp);
         try {
-            TransactionSingleOp.RequestList requests = new TransactionSingleOp.RequestList();
-            for (String scalaris_key : scalaris_keys) {
-                involvedKeys.add(scalaris_key);
-                requests.addRead(scalaris_key);
-            }
-            results = scalaris_single.req_list(requests);
+            executor.run();
         } catch (Exception e) {
             return new ValueResult<List<T>>(false, involvedKeys,
                     "unknown exception reading page list at \""
-                            + scalaris_keys.toString() + "\" from Scalaris: "
-                            + e.getMessage(), false, statName,
-                            System.currentTimeMillis() - timeAtStart);
+                            + involvedKeys.toString() + "\" from Scalaris: "
+                            + e.getMessage(), e instanceof ConnectionException,
+                    statName, System.currentTimeMillis() - timeAtStart);
         }
         
-        List<T> pages = null;
-        int curOp = 0;
-        for (String scalaris_key : scalaris_keys) {
-            try {
-                List<T> pages2 = conv.convert(results.processReadAt(curOp++));
-                if (pages == null) {
-                    pages = pages2;
-                } else {
-                    pages.addAll(pages2);
-                }
-            } catch (NotFoundException e) {
-                if (failNotFound) {
-                    return new ValueResult<List<T>>(false, involvedKeys,
-                            "unknown exception reading page list at \""
-                                    + scalaris_key + "\" from Scalaris: "
-                                    + e.getMessage(), false, statName,
-                                    System.currentTimeMillis() - timeAtStart);
-                }
-            } catch (Exception e) {
-                return new ValueResult<List<T>>(false, involvedKeys,
-                        "unknown exception reading page list at \"" + scalaris_key
-                        + "\" from Scalaris: " + e.getMessage(),
-                        e instanceof ConnectionException, statName,
-                        System.currentTimeMillis() - timeAtStart);
-            }
-        }
-        return new ValueResult<List<T>>(involvedKeys, pages, statName,
+        return new ValueResult<List<T>>(involvedKeys, readOp.getValue(), statName,
                 System.currentTimeMillis() - timeAtStart);
     }
 
@@ -902,29 +798,23 @@ public class ScalarisDataHandler {
      * @return a result object with the page list on success
      */
     public static ValueResult<String> getRandomArticle(Connection connection, Random random) {
-        final long timeAtStart = System.currentTimeMillis();
-        final String statName = "random article";
-        List<String> involvedKeys = new ArrayList<String>();
-        if (connection == null) {
-            return new ValueResult<String>(false, involvedKeys,
-                    "no connection to Scalaris", true, statName,
-                    System.currentTimeMillis() - timeAtStart);
-        }
-        
-        TransactionSingleOp scalaris_single = new TransactionSingleOp(connection);
-        try {
-            // retrieve pages in main namespace
-            final String scalaris_key = getPageListKey(0);
-            involvedKeys.add(scalaris_key);
-            List<ErlangValue> pages = scalaris_single.read(scalaris_key).listValue();
-            String randomTitle = pages.get(random.nextInt(pages.size())).stringValue();
-            return new ValueResult<String>(involvedKeys, randomTitle, statName,
-                    System.currentTimeMillis() - timeAtStart);
-        } catch (Exception e) {
-            return new ValueResult<String>(false, involvedKeys,
-                    "unknown exception reading page list at \"pages\" from Scalaris: "
-                            + e.getMessage(), e instanceof ConnectionException,
-                    statName, System.currentTimeMillis() - timeAtStart);
+        ValueResult<List<ErlangValue>> result = getPageList3(connection,
+                ScalarisOpType.PAGE_LIST, Arrays.asList(getPageListKey(0)),
+                true, "random article",
+                new ErlangConverter<List<ErlangValue>>() {
+                    @Override
+                    public List<ErlangValue> convert(ErlangValue v)
+                            throws ClassCastException {
+                        return v.listValue();
+                    }
+                });
+        if (result.success) {
+            String randomTitle = result.value.get(
+                    random.nextInt(result.value.size())).stringValue();
+            return new ValueResult<String>(result.involvedKeys, randomTitle);
+        } else {
+            return new ValueResult<String>(false, result.involvedKeys,
+                    result.message, result.connect_failed);
         }
     }
 
@@ -1358,7 +1248,7 @@ public class ScalarisDataHandler {
      */
     public static ValueResult<Integer> updatePageList(Transaction scalaris_tx,
             ScalarisOpType opType, String pageList_key, String pageCount_key,
-            List<String> entriesToRemove, List<String> entriesToAdd,
+            List<String> entriesToAdd, List<String> entriesToRemove,
             final String statName) {
         final long timeAtStart = System.currentTimeMillis();
         List<String> involvedKeys = new ArrayList<String>();

contrib/wikipedia/scalaris-wiki/WEB-INF/web.xml
@@ -8,10 +8,6 @@
 		<servlet-name>WikiServletScalaris</servlet-name>
 		<servlet-class>de.zib.scalaris.examples.wikipedia.bliki.WikiServletScalaris</servlet-class>
 		<init-param>
-			<param-name>WIKI_USE_NEW_SCALARIS_OPS</param-name>
-			<param-value>true</param-value>
-		</init-param>
-		<init-param>
 			<param-name>WIKI_USE_BACKLINKS</param-name>
 			<param-value>true</param-value>
 		</init-param>
@@ -27,9 +23,40 @@
 			<param-name>WIKI_REBUILD_PAGES_CACHE</param-name>
 			<param-value>600</param-value>
 		</init-param>
+        <init-param>
+            <param-name>WIKI_STORE_CONTRIBUTIONS</param-name>
+            <param-value>OUTSIDE_TX</param-value>
+        </init-param>
 		<init-param>
-			<param-name>WIKI_STORE_CONTRIBUTIONS</param-name>
-			<param-value>OUTSIDE_TX</param-value>
+			<param-name>WIKI_OPTIMISATIONS</param-name>
+<!--
+optimisations are executed in the same order as mentioned
+format: "|"-separated list of the following (no spaces!): 
+ <operation>:<optimisation>([<parameter>[,<parameter>[,<parameter>]...]])
+available operations:
+ to set all to one optimisation, use:
+  ALL
+ otherwise use values from de.zib.scalaris.examples.wikipedia.ScalarisOpType:
+  PAGE_LIST,
+  CATEGORY_PAGE_LIST,
+  TEMPLATE_PAGE_LIST,
+  BACKLINK_PAGE_LIST,
+  SHORTREV_LIST,
+  ARTICLE_COUNT,
+  PAGE,
+  REVISION,
+  CONTRIBUTION,
+  EDIT_STAT
+available optimisations (parameter types depend on the implementation):
+ see implementations of de.zib.scalaris.examples.wikipedia.Options.ListOptimisation
+  TRADITIONAL
+  APPEND_INCREMENT
+  APPEND_INCREMENT_BUCKETS_WITH_HASH(int buckets)
+
+example:
+ <param-value>ALL:APPEND_INCREMENT|PAGE_LIST:APPEND_INCREMENT_BUCKETS_WITH_HASH(10)</param-value>
+-->
+			<param-value>ALL:APPEND_INCREMENT</param-value>
 		</init-param>
 	</servlet>
 	<servlet-mapping>

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/MyScalarisOpExecWrapper.java
@@ -2,9 +2,13 @@ package de.zib.scalaris.examples.wikipedia;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
-import de.zib.scalaris.examples.wikipedia.ScalarisDataHandler.ScalarisOpType;
+import de.zib.scalaris.examples.wikipedia.Options.APPEND_INCREMENT;
+import de.zib.scalaris.examples.wikipedia.Options.APPEND_INCREMENT_BUCKETS_WITH_HASH;
+import de.zib.scalaris.examples.wikipedia.Options.Optimisation;
 import de.zib.scalaris.executor.ScalarisIncrementOp1;
 import de.zib.scalaris.executor.ScalarisIncrementOp2;
 import de.zib.scalaris.executor.ScalarisOpExecutor;
@@ -56,14 +60,7 @@ public class MyScalarisOpExecWrapper {
      */
     @SuppressWarnings("unchecked")
     public <T> void addAppend(ScalarisOpType opType, String key, T toAdd, String countKey) {
-        switch (opType) {
-        default:
-            if (Options.WIKI_USE_NEW_SCALARIS_OPS) {
-                executor.addOp(new ScalarisListAppendRemoveOp2<T>(key, Arrays.asList(toAdd), new ArrayList<T>(0), countKey));
-            } else {
-                executor.addOp(new ScalarisListAppendRemoveOp1<T>(key, Arrays.asList(toAdd), new ArrayList<T>(0), countKey));
-            }
-        }
+        addAppendRemove(opType, key, Arrays.asList(toAdd), new ArrayList<T>(0), countKey);
     }
 
     /**
@@ -79,14 +76,7 @@ public class MyScalarisOpExecWrapper {
      */
     @SuppressWarnings("unchecked")
     public <T> void addRemove(ScalarisOpType opType, String key, T toRemove, String countKey) {
-        switch (opType) {
-        default:
-            if (Options.WIKI_USE_NEW_SCALARIS_OPS) {
-                executor.addOp(new ScalarisListAppendRemoveOp2<T>(key, new ArrayList<T>(0), Arrays.asList(toRemove), countKey));
-            } else {
-                executor.addOp(new ScalarisListAppendRemoveOp1<T>(key, new ArrayList<T>(0), Arrays.asList(toRemove), countKey));
-            }
-        }
+        addAppendRemove(opType, key, new ArrayList<T>(0), Arrays.asList(toRemove), countKey);
     }
 
     /**
@@ -98,14 +88,17 @@ public class MyScalarisOpExecWrapper {
      * 
      * @param <T>       type of the value to add
      */
-    public <T extends Number> void addIncrement(ScalarisOpType opType, String key, T toAdd) {
-        switch (opType) {
-        default:
-            if (Options.WIKI_USE_NEW_SCALARIS_OPS) {
-                executor.addOp(new ScalarisIncrementOp2<T>(key, toAdd));
-            } else {
-                executor.addOp(new ScalarisIncrementOp1<T>(key, toAdd));
-            }
+    public <T extends Number> void addIncrement(final ScalarisOpType opType,
+            final String key, final T toAdd) {
+        final Optimisation optimisation = Options.OPTIMISATIONS.get(opType);
+        if (optimisation instanceof APPEND_INCREMENT) {
+            executor.addOp(new ScalarisIncrementOp2<T>(key, toAdd));
+        } else if (optimisation instanceof APPEND_INCREMENT_BUCKETS_WITH_HASH) {
+            final APPEND_INCREMENT_BUCKETS_WITH_HASH optimisation2 = (APPEND_INCREMENT_BUCKETS_WITH_HASH) optimisation;
+            final String key2 = key + optimisation2.getBucketString(toAdd);
+            executor.addOp(new ScalarisIncrementOp2<T>(key2, toAdd));
+        } else {
+            executor.addOp(new ScalarisIncrementOp1<T>(key, toAdd));
         }
     }
 
@@ -128,17 +121,46 @@ public class MyScalarisOpExecWrapper {
      * 
      * @param <T>       type of the value to remove
      */
-    public <T> void addAppendRemove(ScalarisOpType opType, String key,
-            List<T> toAdd, List<T> toRemove, String countKey) {
-        switch (opType) {
-        default:
-            if (Options.WIKI_USE_NEW_SCALARIS_OPS) {
-                executor.addOp(new ScalarisListAppendRemoveOp2<T>(key, toAdd, toRemove, countKey));
-            } else {
-                executor.addOp(new ScalarisListAppendRemoveOp1<T>(key, toAdd, toRemove, countKey));
+    public <T> void addAppendRemove(final ScalarisOpType opType,
+            final String key, final List<T> toAdd, final List<T> toRemove,
+            final String countKey) {
+
+        final Optimisation optimisation = Options.OPTIMISATIONS.get(opType);
+        if (optimisation instanceof APPEND_INCREMENT) {
+            executor.addOp(new ScalarisListAppendRemoveOp2<T>(key, toAdd, toRemove, countKey));
+        } else if (optimisation instanceof APPEND_INCREMENT_BUCKETS_WITH_HASH) {
+            final APPEND_INCREMENT_BUCKETS_WITH_HASH optimisation2 = (APPEND_INCREMENT_BUCKETS_WITH_HASH) optimisation;
+            final HashMap<String, String> countKeys = new HashMap<String, String>(toAdd.size() + toRemove.size());
+            final LinkedMultiHashMap<String, T> kvAdd = new LinkedMultiHashMap<String, T>();
+            final LinkedMultiHashMap<String, T> kvRemove = new LinkedMultiHashMap<String, T>();
+            for (T t : toAdd) {
+                final String bucketStr = optimisation2.getBucketString(t);
+                final String key2 = key + bucketStr;
+                final String countKey2 = countKey + bucketStr;
+                countKeys.put(key2, countKey2);
+                kvAdd.put(key2, t);
             }
+            for (T t : toRemove) {
+                final String bucketStr = optimisation2.getBucketString(t);
+                final String key2 = key + bucketStr;
+                final String countKey2 = countKey + bucketStr;
+                countKeys.put(key2, countKey2);
+                kvRemove.put(key2, t);
+            }
+            for (Entry<String, String> entry : countKeys.entrySet()) {
+                final String key2 = entry.getKey();
+                List<T> toAdd2 = kvAdd.get(key2);
+                if (toAdd2 == null) {
+                    toAdd2 = new ArrayList<T>(0);
+                }
+                List<T> toRemove2 = kvRemove.get(key2);
+                if (toRemove2 == null) {
+                    toRemove2 = new ArrayList<T>(0);
+                }
+                executor.addOp(new ScalarisListAppendRemoveOp2<T>(key2, toAdd2, toRemove2, entry.getValue()));
+            }
+        } else {
+            executor.addOp(new ScalarisListAppendRemoveOp1<T>(key, toAdd, toRemove, countKey));
         }
-        // TODO Auto-generated method stub
-        
     }
 }

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/MyScalarisSingleOpExecutor.java
@@ -1,0 +1,87 @@
+/**
+ *  Copyright 2012 Zuse Institute Berlin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package de.zib.scalaris.examples.wikipedia;
+
+import java.util.List;
+
+import com.ericsson.otp.erlang.OtpErlangException;
+
+import de.zib.scalaris.AbortException;
+import de.zib.scalaris.ConnectionException;
+import de.zib.scalaris.RequestList;
+import de.zib.scalaris.TimeoutException;
+import de.zib.scalaris.TransactionSingleOp;
+import de.zib.scalaris.TransactionSingleOp.ResultList;
+import de.zib.scalaris.UnknownException;
+import de.zib.scalaris.executor.ScalarisOp;
+import de.zib.scalaris.executor.ScalarisSingleOpExecutor;
+
+/**
+ * Executes multiple {@link ScalarisOp} operations in multiple phases only
+ * sending requests to Scalaris once per work phase.
+ * 
+ * In addition to {@link ScalarisSingleOpExecutor}, also collects info about all
+ * involved keys.
+ * 
+ * @author Nico Kruber, kruber@zib.de
+ */
+public class MyScalarisSingleOpExecutor extends ScalarisSingleOpExecutor {
+    protected final List<String> involvedKeys;
+
+    /**
+     * Creates a new executor.
+     * 
+     * @param scalaris_single
+     *            the Scalaris connection to use
+     * @param involvedKeys
+     *            list of all involved keys
+     */
+    public MyScalarisSingleOpExecutor(TransactionSingleOp scalaris_single,
+            List<String> involvedKeys) {
+        super(scalaris_single);
+        this.involvedKeys = involvedKeys;
+    }
+
+    /**
+     * Executes the given requests and records all involved keys.
+     * 
+     * @param requests
+     *            a request list to execute
+     * 
+     * @return the results from executing the requests
+     * 
+     * @throws OtpErlangException
+     *             if an error occurred verifying a result from previous
+     *             operations
+     * @throws UnknownException
+     *             if an error occurred verifying a result from previous
+     *             operations
+     */
+    @Override
+    protected ResultList executeRequests(RequestList requests)
+            throws ConnectionException, TimeoutException, AbortException,
+            UnknownException {
+        involvedKeys.addAll(requests.keyList());
+        return super.executeRequests(requests);
+    }
+
+    /**
+     * @return the involvedKeys
+     */
+    public List<String> getInvolvedKeys() {
+        return involvedKeys;
+    }
+}

commit 4c3eca96e0f34f16f943b8c1fe4992951bcdc94e

- Wiki on Scalaris: fixed random times not set (regression introduced in r3003)
git-svn-id: https://scalaris.googlecode.com/svn/trunk@3428 71766da9-9752-0410-ab17-bfafa751c56a

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/ScalarisDataHandler.java
@@ -398,15 +398,18 @@ public class ScalarisDataHandler {
                         return v.listValue();
                     }
                 });
+        ValueResult<NormalisedTitle> vResult;
         if (result.success) {
             String randomTitle = result.value.get(
                     random.nextInt(result.value.size())).stringValue();
-            return new ValueResult<NormalisedTitle>(result.involvedKeys,
+            vResult = new ValueResult<NormalisedTitle>(result.involvedKeys,
                     NormalisedTitle.fromNormalised(randomTitle));
         } else {
-            return new ValueResult<NormalisedTitle>(false, result.involvedKeys,
-                    result.message, result.connect_failed);
+            vResult = new ValueResult<NormalisedTitle>(false,
+                    result.involvedKeys, result.message, result.connect_failed);
         }
+        vResult.stats = result.stats;
+        return vResult;
     }
 
     /**