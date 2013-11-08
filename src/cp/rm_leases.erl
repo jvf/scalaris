jvf/scalaris

commit 6d821d4c2ac7f084a80bd16d4c003f5bd319f892

make leases optional
- nodes have at most one active lease
- move lease list structure into module
- start ring maintenance for leases


git-svn-id: https://scalaris.googlecode.com/svn/trunk@5670 71766da9-9752-0410-ab17-bfafa751c56a

src/cp/rm_leases.erl
@@ -1,0 +1,104 @@
+% @copyright 2007-2013 Zuse Institute Berlin
+
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+
+%% @author Thorsten Schuett <schuett@zib.de>
+%% @doc    Ring maintenance with leases.
+%% @end
+%% @version $$
+-module(rm_leases).
+-author('schuett@zib.de').
+-vsn('$Id$').
+
+-behaviour(gen_component).
+-include("scalaris.hrl").
+-include("record_helpers.hrl").
+
+%% gen_component callbacks
+-export([start_link/1, init/1, on/2]).
+
+-type state() :: op_t() | ok.
+
+-record(op, {
+          missing_range = ?required(op, missing_range) :: intervals:interval(),
+          found_leases  = ?required(op, found_leases) :: list(l_on_cseq:lease_t())
+         }).
+
+-type op_t() :: #op{}.
+
+%% gen_component functions
+%% @doc Starts the failure detector server
+-spec start_link(pid_groups:groupname()) -> {ok, pid()} | ignore.
+start_link(ServiceGroup) ->
+    case config:read(leases) of
+        true ->
+            gen_component:start_link(?MODULE, fun ?MODULE:on/2, [],
+                                     [wait_for_init, {erlang_register, ?MODULE},
+                                      {pid_groups_join_as, ServiceGroup, ?MODULE}]);
+        _ ->
+            ignore
+    end.
+
+%% @doc Initialises the module with an empty state.
+-spec init([]) -> state().
+init([]) ->
+    FilterFun = fun (Old, New, Reason) ->
+                        OldRange = nodelist:node_range(Old),
+                        NewRange = nodelist:node_range(New),
+                        %case OldRange =/= NewRange of
+                        %    true ->
+                        %        log:log("the range has changed: ~w -> ~w (~w)",
+                        %                [OldRange, NewRange, Reason]);
+                        %    false ->
+                        %        ok
+                        %end,
+                        case Reason of
+                            {slide_finished, _} ->
+                                false;
+                            {add_subscriber} ->
+                                false;
+                            {node_crashed, _} ->
+                                OldRange =/= NewRange;
+                            {node_discovery} ->
+                                OldRange =/= NewRange;
+                            {unknown} -> % @todo ?
+                                false
+                        end
+                end,
+    ExecFun = fun (Pid, _Tag, Old, New) ->
+                      comm:send_local(Pid, {rm_change, nodelist:node_range(Old), nodelist:node_range(New)})
+              end,
+    rm_loop:subscribe(self(), ?MODULE, FilterFun, ExecFun, inf),
+    ok.
+
+%% @private
+-spec on(comm:message(), state()) -> state().
+on({rm_change, OldRange, NewRange}, State) ->
+    log:log("the range has changed: ~w -> ~w", [OldRange, NewRange]),
+    log:log("state: ~w", [State]),
+    case State of
+        ok ->
+            MissingRange = intervals:minus(NewRange, OldRange),
+            log:log("missing range: ~w", [MissingRange]),
+            #op{missing_range = MissingRange, found_leases = []};
+        _ ->
+            State
+    end;
+
+on({merge_after_leave, NewLease, OldLease, Result}, State) ->
+    log:log("merge after finish done: ~w", [Result]),
+    State.
+
+
+
+

src/cp/leases.erl
@@ -37,35 +37,40 @@
 
 -spec is_responsible(dht_node_state:state(), ?RT:key()) -> boolean() | maybe.
 is_responsible(State, Key) ->
-    {ActiveLeaseList, _} = dht_node_state:get(State, lease_list),
-    is_responsible_(ActiveLeaseList, Key).
-%    case lists:any(fun (Lease) ->
-%                           intervals:in(Key, l_on_cseq:get_range(Lease))
-%                               andalso l_on_cseq:is_valid(Lease)
-%                   end, ActiveLeaseList) of
-%        true -> true;
-%        false ->
-%            %log:log("is_responsible failed ~p ~p", [Key, ActiveLeaseList]),
-%            false
-%    end.
+    LeaseList = dht_node_state:get(State, lease_list),
+    is_responsible_(LeaseList, Key).
 
-is_responsible_([], _Key) ->
-    false;
-is_responsible_([Lease|List], Key) ->
-    RangeMatches = intervals:in(Key, l_on_cseq:get_range(Lease)),
-    IsAlive = l_on_cseq:is_valid(Lease),
-    if
-        IsAlive andalso RangeMatches ->
-            true;
-        RangeMatches ->
-            case is_responsible_(List, Key) of
-                true ->
+-spec is_responsible_(lease_list:lease_list(), ?RT:key()) -> boolean() | maybe.
+is_responsible_(LeaseList, Key) ->
+    ActiveLease = lease_list:get_active_lease(LeaseList),
+    %PassiveLease = lease_list:get_passive_leases(LeaseList),
+    case ActiveLease of
+        empty ->
+            PassiveLeases = lease_list:get_passive_leases(LeaseList),
+            is_responsible_passive_leases(PassiveLeases, Key);
+        _ ->
+            RangeMatches = intervals:in(Key, l_on_cseq:get_range(ActiveLease)),
+            IsAlive = l_on_cseq:is_valid(ActiveLease),
+            if
+                IsAlive andalso RangeMatches ->
                     true;
-                maybe ->
+                RangeMatches ->
+                    log:log("returned maybe"),
                     maybe;
-                false ->
-                    maybe
-            end;
+                true ->
+                    %log:log("returned false"),
+                    PassiveLeases = lease_list:get_passive_leases(LeaseList),
+                    is_responsible_passive_leases(PassiveLeases, Key)
+            end
+    end.
+
+-spec is_responsible_passive_leases(list(l_on_cseq:lease_t()), ?RT:key()) -> boolean() | maybe.
+is_responsible_passive_leases([], _Key) ->
+    false;
+is_responsible_passive_leases([Lease|Rest], Key) ->
+    case intervals:in(Key, l_on_cseq:get_range(Lease)) of
         true ->
-            is_responsible_(List, Key)
+            maybe;
+        false ->
+            is_responsible_passive_leases(Rest, Key)
     end.

src/dht_node_state.erl
@@ -106,7 +106,7 @@
                 lease_db2 = ?required(state, prbr_state) :: prbr:state(),
                 lease_db3 = ?required(state, prbr_state) :: prbr:state(),
                 lease_db4 = ?required(state, prbr_state) :: prbr:state(),
-                lease_list = ?required(state, lease_list) :: l_on_cseq:lease_list_state(),
+                lease_list = ?required(state, lease_list) :: lease_list:lease_list(),
                 snapshot_state   = null :: snapshot_state:snapshot_state() | null,
                 mr_state   = ?required(state, mr_state)  :: orddict:orddict()
                }).
@@ -131,8 +131,8 @@ new(RT, RMState, DB) ->
            lease_db2 = prbr:init(lease_db2),
            lease_db3 = prbr:init(lease_db3),
            lease_db4 = prbr:init(lease_db4),
-           lease_list = l_on_cseq:empty_lease_list(),
-		   snapshot_state = snapshot_state:new(),
+           lease_list = lease_list:empty(),
+           snapshot_state = snapshot_state:new(),
            mr_state = orddict:new()
           }.
 

src/slide_leases.erl
@@ -96,7 +96,7 @@ update_rcv_data2(State, SlideOp, {continue}) ->
 prepare_send_delta1(State, OldSlideOp, ReplyPid) ->
     % start to split own range
     log:log("prepare_send_delta1 ~p~n", [slide_op:get_type(OldSlideOp)]),
-    case find_lease(State, OldSlideOp) of
+    case find_lease(State, OldSlideOp, active) of
         {ok, Lease} ->
             Id = l_on_cseq:id(l_on_cseq:get_range(Lease)),
             % check slide direction
@@ -106,16 +106,17 @@ prepare_send_delta1(State, OldSlideOp, ReplyPid) ->
                     {R1, R2} = case intervals:in(Id, Interval) of
                                    true ->
                                                 % ->
+                                       true = false, % TS: not tested
                                        {intervals:minus(l_on_cseq:get_range(Lease), Interval), Interval};
                                    false ->
                                                 % <-
                                        {Interval, intervals:minus(l_on_cseq:get_range(Lease), Interval)}
                                end,
                     NewOwner = node:pidX(slide_op:get_node(OldSlideOp)),
-                    l_on_cseq:lease_split_and_change_owner(Lease, R1, R2, NewOwner, ReplyPid),
+                    l_on_cseq:lease_split_and_change_owner(Lease, R1, R2, second, NewOwner, ReplyPid),
                     {ok, State, OldSlideOp};
                 true ->
-                    %log:log("only change owner instead of split and change owner", []),
+                    log:log("only change owner instead of split and change owner", []),
                     %% @todo
                     %log:log("Id ~p ~n", [Id]),
                     %log:log("Interval     ~p", [Interval]),
@@ -145,11 +146,11 @@ prepare_send_delta1(State, OldSlideOp, ReplyPid) ->
 prepare_send_delta2(State, SlideOp, Msg) ->
     % check that split has been done
     case Msg of
-        {handover, success, NewLease} ->
+        {handover, success, _NewLease} ->
             % disable new lease
             log:log("prepare_send_delta2 ~p~n", [Msg]),
-            State1 = locally_disable_lease(State, NewLease),
-            {ok, State1, SlideOp};
+            %State1 = locally_disable_lease(State, NewLease),
+            {ok, State, SlideOp};
         {split, fail, _Lease} ->
             log:log("prepare_send_delta2: split failed~n", []),
             % @todo
@@ -196,13 +197,14 @@ finish_delta_ack2(State, SlideOp, NextOpMsg, Msg) ->
     % notify neighbor on successful handover
     log:log("finish_delta_ack2 ~p~n", [Msg]),
     % notify succ
-    case find_lease(State, SlideOp) of
+    case find_lease(State, SlideOp, passive) of
         {ok, Lease} ->
             Owner = l_on_cseq:get_owner(Lease),
             l_on_cseq:lease_send_lease_to_node(Owner, Lease),
-            State1 = l_on_cseq:remove_lease_from_dht_node_state(Lease, State),
+            State1 = lease_list:remove_lease_from_dht_node_state(Lease, State, passive),
             {ok, State1, SlideOp, NextOpMsg};
         error ->
+            log:log("error in finish_delta_ack2"),
             % @todo
             error
     end.
@@ -223,21 +225,31 @@ send_continue_msg(Pid) ->
 locally_disable_lease(State, Lease) ->
     l_on_cseq:disable_lease(State, Lease).
 
-find_lease(State, SlideOp) ->
-    {ActiveLeaseList, PassiveLeaseList} = dht_node_state:get(State, lease_list),
+find_lease(State, SlideOp, Mode) ->
+    LeaseList = dht_node_state:get(State, lease_list),
     Interval = slide_op:get_interval(SlideOp),
     Pred = fun (L) ->
                    intervals:is_subset(Interval, l_on_cseq:get_range(L))
-                       andalso intervals:is_continuous(intervals:intersection(Interval, l_on_cseq:get_range(L)))
+                       andalso intervals:is_continuous(
+                                   intervals:intersection(Interval,l_on_cseq:get_range(L)))
            end,
-    case lists:filter(Pred, ActiveLeaseList) of
-        [Lease] ->
-            {ok, Lease};
-        _ ->
-            case lists:filter(Pred, PassiveLeaseList) of
+    ActiveLease = lease_list:get_active_lease(LeaseList),
+    PassiveLeases = lease_list:get_passive_leases(LeaseList),
+    case Mode of
+        active ->
+            case Pred(ActiveLease) of
+                true ->
+                    {ok, ActiveLease};
+                false ->
+                    error
+            end;
+        passive ->
+            case lists:filter(Pred, PassiveLeases) of
                 [Lease] ->
                     {ok, Lease};
                 _ ->
+                    log:log("did not found requested lease in passive list: ~w ~w",
+                            [Interval, PassiveLeases]),
                     error
             end
     end.

src/sup_dht_node.erl
@@ -57,11 +57,11 @@ supspec(_) ->
 -spec childs([{pid_groups:groupname(), Options::[tuple()]}]) ->
                     [ProcessDescr::supervisor:child_spec()].
 childs([{DHTNodeGroup, Options}]) ->
-	Autoscale =
-		case config:read(autoscale) of
-			true -> sup:worker_desc(autoscale, autoscale, start_link, [DHTNodeGroup]);
-			_ -> []
-		end,
+    Autoscale =
+        case config:read(autoscale) of
+            true -> sup:worker_desc(autoscale, autoscale, start_link, [DHTNodeGroup]);
+            _ -> []
+        end,
     Cyclon = sup:worker_desc(cyclon, cyclon, start_link, [DHTNodeGroup]),
     DBValCache =
         sup:worker_desc(dht_node_db_cache, dht_node_db_cache, start_link,
@@ -80,6 +80,9 @@ childs([{DHTNodeGroup, Options}]) ->
     Reregister =
         sup:worker_desc(dht_node_reregister, dht_node_reregister,
                              start_link, [DHTNodeGroup]),
+    RMLeases =
+        sup:worker_desc(rm_leases, rm_leases,
+                             start_link, [DHTNodeGroup]),
     RoutingTable =
         sup:worker_desc(routing_table, rt_loop, start_link,
                              [DHTNodeGroup]),
@@ -87,7 +90,7 @@ childs([{DHTNodeGroup, Options}]) ->
         sup:supervisor_desc(sup_dht_node_core, sup_dht_node_core,
                                  start_link, [DHTNodeGroup, Options]),
     %% SupMr = case config:read(mr_enable) of
-    %%     true -> 
+    %%     true ->
     %%         util:sup_supervisor_desc(sup_mr, sup_mr, start_link, [DHTNodeGroup]);
     %%     _ -> []
     %% end,
@@ -104,10 +107,10 @@ childs([{DHTNodeGroup, Options}]) ->
         end,
     SnapshotLeader =
         sup:worker_desc(snapshot_leader, snapshot_leader, start_link),
-    SupWPool = 
+    SupWPool =
         sup:supervisor_desc(sup_wpool, sup_wpool, start_link, [DHTNodeGroup]),
     WPool = sup:worker_desc(wpool, wpool, start_link, [DHTNodeGroup, Options]),
-	
+
     lists:flatten([ %% RepUpd may be [] and lists:flatten eliminates this
                     Monitor,
                     Delayer,
@@ -124,6 +127,7 @@ childs([{DHTNodeGroup, Options}]) ->
                     WPool,
                     MonitorPerf,
                     RepUpdate,
-					Autoscale,
-                    SnapshotLeader
+                    Autoscale,
+                    SnapshotLeader,
+                    RMLeases
            ]).

test/prbr_SUITE.erl
@@ -178,7 +178,7 @@ rbr_concurrency_leases(_Config) ->
                                               l_on_cseq:unittest_lease_update(
                                                 V,
                                                 l_on_cseq:set_version(
-                                                  V, l_on_cseq:get_version(V)+1)),
+                                                  V, l_on_cseq:get_version(V)+1), passive),
                                           case Update of
                                               ok -> ok;
                                               failed ->

commit 11aade4158e1b3e096f09d9aafae571117f92130

rm_leases: fix compilation on Erlang R13 (broken in r5670)
git-svn-id: https://scalaris.googlecode.com/svn/trunk@5686 71766da9-9752-0410-ab17-bfafa751c56a

src/cp/rm_leases.erl
@@ -27,14 +27,14 @@
 %% gen_component callbacks
 -export([start_link/1, init/1, on/2]).
 
--type state() :: op_t() | ok.
-
 -record(op, {
           missing_range = ?required(op, missing_range) :: intervals:interval(),
           found_leases  = ?required(op, found_leases) :: list(l_on_cseq:lease_t())
          }).
 
 -type op_t() :: #op{}.
+
+-type state() :: op_t() | ok.
 
 %% gen_component functions
 %% @doc Starts the failure detector server