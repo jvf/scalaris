7b6a62a9c535fdb91b6843e765dfdef4b7e9c67a
jensvfischer <jensvfischer@gmail.com>
2016-02-23 12:27:25 +0100

    Revert "debug: watchdog"
    This reverts commit ac8b31f8f9e44ec89c873c4d38e63002086e365f.

--- contrib/slurm/util/watchdog.sh
+++ contrib/slurm/util/watchdog.sh
@@ -9,27 +9,15 @@ function cleanup_node(){
 IS_RUNNING=("RUNNING")
 SACCT_RES=$(sacct -j $SLURM_JOB_ID -b)
 if [[ $? -eq 0 ]]; then
-    echo "$(date +%y-%m-%d-%H:%M:%S): $(hostname -s): with sacct error" >> /scratch/bzcfisch/slog/watchdog_${SLURM_JOB_ID}
     IS_RUNNING=($(echo "$SACCT_RES" | grep $SLURM_JOB_ID | awk '{print $2}'))
 fi
 while [[ ${IS_RUNNING[0]} == "RUNNING" ]]; do
     sleep $WATCHDOG_INTERVAL
     SACCT_RES=$(sacct -j $SLURM_JOB_ID -b)
     if [[ $? -eq 0 ]]; then
-        echo "$(date +%y-%m-%d-%H:%M:%S): $(hostname -s): with sacct error" >> /scratch/bzcfisch/slog/watchdog_${SLURM_JOB_ID}
         IS_RUNNING=($(echo "$SACCT_RES" | grep $SLURM_JOB_ID | awk '{print $2}'))
     fi
 done
-
-cat <<EOF >> /scratch/bzcfisch/slog/watchdog_${SLURM_JOB_ID}
-$(date +%y-%m-%d-%H:%M:%S): $(hostname -s):
-SACCT_RES:
-$SACCT_RES
-IS_RUNNING: ${IS_RUNNING[@]}
-screen ls:
-$(screen -ls)
-
-EOF
 
 cleanup_node
 
